7ed0962017f7195f3e95fb569116ef6570afaa2b
nightblade9 <nightbladecodes@gmail.com>
2020-01-10 15:54:07 -0500

    Basic unit tests for `Program`!

--- Puffin.CommandLineInterface/Program.cs
+++ Puffin.CommandLineInterface/Program.cs
@@ -3,11 +3,11 @@ using System.IO;
 
 namespace Puffin.CommandLineInterface
 {
-    class Program
+    public class Program
     {
         private const string DEFAULT_NEW_PROJECT_NAME = "NewPuffin";
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // TODO: don't assume the request is a new project
             var projectName = args.Length == 1 ? args[0] : DEFAULT_NEW_PROJECT_NAME;
@@ -21,7 +21,6 @@ namespace Puffin.CommandLineInterface
                 Directory.CreateDirectory(projectName);
                 Console.WriteLine($"Created a new project in the {projectName} directory!");
             }
-            
         }
     }
 }
