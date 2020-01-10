56442b94baa13432327c10fa4ca185e57307cbd9
nightblade9 <nightbladecodes@gmail.com>
2020-01-10 15:44:08 -0500

    Basic CLI: create a new project directory It's empty. Also fail if the directory already exists.

--- Puffin.CommandLineInterface/Program.cs
+++ Puffin.CommandLineInterface/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
 
 namespace Puffin.CommandLineInterface
 {
     class Program
     {
+        private const string DEFAULT_NEW_PROJECT_NAME = "NewPuffin";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TODO: don't assume the request is a new project
+            var projectName = args.Length == 1 ? args[0] : DEFAULT_NEW_PROJECT_NAME;
+
+            if (Directory.Exists(projectName))
+            {
+                Console.WriteLine($"Can't create a new project directory named {projectName}, the directory already exists.");
+            }
+            else
+            {
+                Directory.CreateDirectory(projectName);
+                Console.WriteLine($"Created a new project in the {projectName} directory!");
+            }
+            
         }
     }
 }
