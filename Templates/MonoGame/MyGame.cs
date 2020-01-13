31d427e62a50635aa2a5ee960e628158f17d8408
nightblade9 <nightbladecodes@gmail.com>
2020-01-12 23:13:28 -0500

    Fix: game actually works/draws now Key: calling Base.Initialize in PuffinGame Also: update horrible background to nice colour Also: F5 in VSCode now builds everything

--- Templates/MonoGame/MyGame.cs
+++ Templates/MonoGame/MyGame.cs
@@ -7,7 +7,7 @@ namespace MonoGame
 {
     public class MyGame : PuffinGame
     {
-        override protected void Initialize()
+        override protected void Ready()
         {
             // TODO: don't use references to source for Puffin.Infrastructure.MonoGame!
             this.ShowScene(new CoreGameScene());
