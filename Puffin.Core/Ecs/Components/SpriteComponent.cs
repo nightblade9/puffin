98b7da6194e67e86c90001e1e71aa8dd6cec3698
nightblade9 <nightbladecodes@gmail.com>
2020-01-14 13:16:43 -0500

    WIP for spritesheets

--- Puffin.Core/Ecs/Components/SpriteComponent.cs
+++ Puffin.Core/Ecs/Components/SpriteComponent.cs
@@ -4,6 +4,16 @@ namespace Puffin.Core.Ecs.Components
     {
         public string FileName { get; private set; }
 
+        /// <summary>
+        /// The width of a frame of the sprite (if it's a spritesheet), or zero if not a spritesheet.
+        /// </summary>
+        public int FrameWidth = 0;
+
+        /// <summary>
+        /// The height of a frame of the sprite (if it's a spritesheet), or zero if not a spritesheet.
+        /// </summary>
+        public int FrameHeight = 0;
+
         public SpriteComponent(string fileName)
         {
             this.FileName = fileName;
