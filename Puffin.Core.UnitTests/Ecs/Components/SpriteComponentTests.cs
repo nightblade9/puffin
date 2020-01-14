3c13490f9607816bfbcc7ee76b3c2d19cd033fb1
nightblade9 <nightbladecodes@gmail.com>
2020-01-14 16:38:17 -0500

    MonoGameSprite tracks/updates region based on frame index Frame index validation in `SpriteComponent` `EventBus` always has a default Template game shows a spritesheet changing

---
+++ Puffin.Core.UnitTests/Ecs/Components/SpriteComponentTests.cs
@@ -0,0 +1,40 @@
+using System;
+using NUnit.Framework;
+using Puffin.Core.Ecs;
+using Puffin.Core.Ecs.Components;
+
+namespace Puffin.Core.UnitTests.Ecs.Components
+{
+    [TestFixture]
+    public class SpriteComponentTests
+    {
+        [TestCase(-193)]
+        [TestCase(-1)]
+        public void FrameIndexSetterThrowsIfIndexIsNegative(int index)
+        {
+            var component = new SpriteComponent("galaxy.png", 16, 16);
+            Assert.Throws<ArgumentException>(() => component.FrameIndex = index);
+        }
+
+        public void FrameIndexSetterThrowsIfFrameWidthOrHeightArentSet()
+        {
+            var component = new SpriteComponent("galaxy.png");
+            Assert.Throws<ArgumentException>(() => component.FrameIndex = 2323);
+        }
+
+        [Test]
+        public void FrameIndexChangeBroadcastsFrameIndexChangedEvent()
+        {
+            // Arrange
+            var component = new SpriteComponent("galaxy.png", 32, 32);
+            var called = false;
+            new EventBus().Subscribe("spritesheet frame index changed", (data) => called = true);
+
+            // Act
+            component.FrameIndex = 3;
+
+            // Assert
+            Assert.That(called, Is.True);
+        }
+    }
+}
--- Templates/MonoGame/CoreGameScene.cs
+++ Templates/MonoGame/CoreGameScene.cs
@@ -1,14 +1,24 @@
+using System;
 using Puffin.Core;
 using Puffin.Core.Ecs;
 using Puffin.Core.Ecs.Components;
 
 public class CoreGameScene : Scene
 {
+    private Entity entity;
+
     public CoreGameScene()
     {
-        this.Add(
-            new Entity()
+        this.entity = new Entity()
             .Set(new SpriteComponent("tilemap.png", 32, 32))
-            .Move(300, 200));
+            .Move(300, 200);
+
+        this.Add(this.entity);
+    }
+
+    override public void Update()
+    {
+        var index = DateTime.Now.Second % 4;
+        entity.GetIfHas<SpriteComponent>().FrameIndex = index;
     }
 }
