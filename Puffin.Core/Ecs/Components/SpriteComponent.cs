9e4c28897e83d00c8b7552d9a513dc5ef5cce59b
nightblade9 <nightbladecodes@gmail.com>
2020-01-14 15:28:35 -0500

    Spritesheets work: display the selected region.

--- Puffin.Core/Ecs/Components/SpriteComponent.cs
+++ Puffin.Core/Ecs/Components/SpriteComponent.cs
@@ -2,21 +2,31 @@ namespace Puffin.Core.Ecs.Components
 {
     public class SpriteComponent : Component
     {
-        public string FileName { get; private set; }
+        public readonly string FileName;
 
         /// <summary>
         /// The width of a frame of the sprite (if it's a spritesheet), or zero if not a spritesheet.
         /// </summary>
-        public int FrameWidth = 0;
+        public readonly int FrameWidth;
 
         /// <summary>
         /// The height of a frame of the sprite (if it's a spritesheet), or zero if not a spritesheet.
         /// </summary>
-        public int FrameHeight = 0;
+        public readonly int FrameHeight;
 
         public SpriteComponent(string fileName)
         {
             this.FileName = fileName;
+        }
+
+        /// <summary>
+        /// Used to construct a spritesheet, by passing in the frame width/height.
+        /// </summary>
+        public SpriteComponent(string spritesheetFileName, int frameWidth, int frameHeight)
+        : this(spritesheetFileName)
+        {
+            this.FrameWidth = frameWidth;
+            this.FrameHeight = frameHeight;
         }
     }
 }
--- Puffin.Infrastructure.MonoGame/Drawing/MonoGameSprite.cs
+++ Puffin.Infrastructure.MonoGame/Drawing/MonoGameSprite.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Puffin.Core.Ecs;
@@ -9,6 +10,7 @@ namespace Puffin.Infrastructure.MonoGame.Drawing
     {
         public Texture2D Texture { get; private set; }
         public Vector2 Position { get; private set; }
+        public Rectangle Region { get; private set; }
 
         public MonoGameSprite(Entity parent, Texture2D texture)
         {
@@ -16,6 +18,22 @@ namespace Puffin.Infrastructure.MonoGame.Drawing
             parent.AddPositionChangeCallback((x, y) => this.Position = new Vector2(x, y));
             // Get initial position set correctly
             this.Position = new Vector2(parent.X, parent.Y);
+
+            // if it's a spritesheet, note the rectangle.
+            var sprite = parent.GetIfHas<SpriteComponent>();
+            if (sprite.FrameWidth > 0 && sprite.FrameHeight > 0)
+            {
+                this.Region = new Rectangle(0, 0, sprite.FrameWidth, sprite.FrameHeight);
+            }
+            else if (sprite.FrameWidth == 0 && sprite.FrameHeight == 0)
+            {
+                // Regular image
+                this.Region = new Rectangle(0, 0, texture.Width, texture.Height);
+            }
+            else
+            {
+                throw new InvalidOperationException($"Frame width/height must be positive, they are currently {sprite.FrameWidth}, {sprite.FrameHeight}");
+            }
         }
     }
 }
