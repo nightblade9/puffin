fa5b32a832ce987cd016c7164ac9af460bb52a5f
nightblade9 <nightbladecodes@gmail.com>
2020-01-16 21:34:09 -0500

    Fancy-pants button control now available. Depends on existing sprite (in template project). Added unit tests. Nerf accessibility on DI reset to internal

--- Puffin.Core/AssemblyInfo.cs
+++ Puffin.Core/AssemblyInfo.cs
@@ -2,5 +2,6 @@ using System.Runtime.CompilerServices;
 
 [assembly:InternalsVisibleTo("Puffin.Core.UnitTests")]
 [assembly:InternalsVisibleTo("Puffin.Infrastructure.MonoGame")]
+[assembly:InternalsVisibleTo("Puffin.UI.UnitTests")]
 // For Mock to access IDrawingSurface and other internals
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
