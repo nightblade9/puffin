fbf68b561d9a4f97993416f97a391b6f141794d9
nightblade9 <nightbladecodes@gmail.com>
2020-01-21 20:54:28 -0500

    `OverlapSystem` tests. Simplified + fixed bug in overlap component code.

--- Puffin.Core.UnitTests/Ecs/Systems/OverlapSystemTests.cs
+++ Puffin.Core.UnitTests/Ecs/Systems/OverlapSystemTests.cs
@@ -36,7 +36,31 @@ namespace Puffin.Core.UnitTests.Ecs.Systems
         [Test]
         public void OnUpdateTriggersStoppedOverlapping()
         {
+            var e1Called = false;
+            var e2Called = false;
 
+            // Arrange
+            var e1 = new Entity().Move(16, 16);
+            e1.Set(new OverlapComponent(e1, 32, 32, 0, 0, null, (o) => e1Called = true));
+            
+            var e2 = new Entity();
+            e2.Set(new OverlapComponent(e2, 40, 40, 0, 0, null, (o) => e2Called = true));
+
+            var system = new OverlapSystem();
+            system.OnAddEntity(e1);
+            system.OnAddEntity(e2);
+            
+            // Trigger overlap
+            system.OnUpdate(TimeSpan.Zero);
+
+            e1.Move(-100, -100);
+
+            // Act
+            system.OnUpdate(TimeSpan.Zero);
+
+            // Assert
+            Assert.That(e1Called, Is.True);
+            Assert.That(e2Called, Is.True);
         }
     }
 }
