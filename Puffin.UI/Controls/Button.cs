803f0d1ed1eaa023b5b0eb05928190df4b20991f
nightblade9 <nightbladecodes@gmail.com>
2020-01-29 14:57:27 -0500

    Add missing docs for tilemap/button

--- Puffin.UI/Controls/Button.cs
+++ Puffin.UI/Controls/Button.cs
@@ -3,11 +3,20 @@ using System;
 
 namespace Puffin.UI.Controls
 {
+    /// <summary>
+    /// Creates a clickable button backed with specified image and caption.
+    /// By default, uses the image in <c>Content/Puffin/UI/Button.png</c> as its image.
+    /// </summary>
     public class Button : Entity
     {
         private const int SPRITE_WIDTH = 128;
         private const int SPRITE_HEIGHT = 48;
 
+        /// <summary>
+        /// Creates a new button.
+        /// </summary>
+        /// <param name="text">The text label on the button (if any).</param>
+        /// <param name="onClick">The callback to trigger when the player clicks on the button.</param>
         public Button(string text, Action onClick)
         {
             this.Sprite("Content/Puffin/UI/Button.png");
