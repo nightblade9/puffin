511f1906731bb748a45ec548f258d854901d8a30
nightblade9 <nightbladecodes@gmail.com>
2020-02-24 13:04:56 -0500

    Add missing docs for event bus

--- Puffin.Core/Events/EventBus.cs
+++ Puffin.Core/Events/EventBus.cs
@@ -5,11 +5,13 @@ namespace Puffin.Core.Events
 {
     /// <summary>
     /// An event bus; used internally by Puffin. You can use it to subscribe to and broadcast events.
+    /// Event buses life-cycles are tied to a specific scene; changing scene, creates a new event bus.
     /// Events must be an enum type, and the callbacks must accept a single object as the data.
     /// Note that the event bus is disposed and recreated whenever a scene changes.
     /// </summary>
     public class EventBus : IDisposable
     {
+        /// <summary>Returns the latest instance of the event bus. Showing a new scene creates a new event-bus.</summary>
         public static EventBus LatestInstance { get; private set; } = new EventBus();
 
         // event name => callbacks. Each callback has an optional parameter (data).
@@ -20,6 +22,11 @@ namespace Puffin.Core.Events
             LatestInstance = this;
         }
 
+        /// <summary>
+        /// Broadcast a new event to all subscribers.
+        /// </summary>
+        /// <param name="signal">The enum value of the signal. Any enum works.</param>
+        /// <param name="data">Any data to pass to subscribres (can be null).</param>
         public void Broadcast(Enum signal, object data = null)
         {
             if (subscribers.ContainsKey(signal))
@@ -34,6 +41,11 @@ namespace Puffin.Core.Events
             }
         }
 
+        /// <summary>
+        /// Subscribe a callback to a specific signal. The callback must receive an object (optional data).
+        /// </summary>
+        /// <param name="signal">The enum value of the signal. Any enum works.</param>
+        /// <param name="callback">The callback to invoke whenever the signal is broadcast.</param>
         public void Subscribe(Enum signal, Action<object> callback)
         {
             if (!subscribers.ContainsKey(signal))
@@ -44,6 +56,11 @@ namespace Puffin.Core.Events
             subscribers[signal].Add(callback);
         }
 
+        /// <summary>
+        /// Unsubscribe a callback to a specific signal.
+        /// </summary>
+        /// <param name="signal">The enum value of the signal. Any enum works.</param>
+        /// <param name="callback">The callback to unsubscribe from the specified signal.</param>
         public void Unsubscribe(Enum signal, Action<object> callback)
         {
             if (subscribers.ContainsKey(signal))
--- Puffin.Core/IO/PuffinAction.cs
+++ Puffin.Core/IO/PuffinAction.cs
@@ -1,5 +1,8 @@
 namespace Puffin.Core.IO
 {
+    /// <summary>
+    /// Internal puffin actions; you can subscribe to them to do things like tile-based character movement.
+    /// </summary>
     public enum PuffinAction
     {
 
