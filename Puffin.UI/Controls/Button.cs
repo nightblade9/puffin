2d303f16fae2f2647477fa42372bcae2b8713bc1
nightblade9 <nightbladecodes@gmail.com>
2020-04-21 12:03:52 -0400

    Remove hard-coded button width/height. It needs a bigger fix though.

--- Puffin.UI/Controls/Button.cs
+++ Puffin.UI/Controls/Button.cs
@@ -10,20 +10,18 @@ namespace Puffin.UI.Controls
     /// </summary>
     public class Button : Entity
     {
-        private const int SPRITE_WIDTH = 128;
-        private const int SPRITE_HEIGHT = 48;
-
         /// <summary>
         /// Creates a new button.
         /// </summary>
         /// <param name="text">The text label on the button (if any).</param>
         /// <param name="onClick">The callback to trigger when the player clicks on the button.</param>
-        public Button(bool isUiElement, string imagePath, string text, int textXOffset, int textYOffset, Action<int, int> onClick)
+        public Button(bool isUiElement, string imagePath, int spriteWidth, int spriteHeight, string text, int textXOffset, int textYOffset, Action<int, int> onClick)
         : base(isUiElement)
         {
+            // Get rid of sprite width/height once this.Sprite(...) is enough to get the sprite width/height
             this.Sprite(imagePath);
             this.Label(text, textXOffset, textYOffset);
-            this.Mouse(SPRITE_WIDTH, SPRITE_HEIGHT, onClick);
+            this.Mouse(spriteWidth, spriteHeight, onClick);
         }
     }
 }
