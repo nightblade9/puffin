8786e52346106cfe287d540a0ef6215f143e14db
nightblade9 <nightbladecodes@gmail.com>
2020-07-16 19:29:07 -0400

    Refactor: clean up volume/pitch in AudioComponent Cleaner API for setting values before play.

--- Puffin.Core/Ecs/Components/AudioComponent.cs
+++ Puffin.Core/Ecs/Components/AudioComponent.cs
@@ -5,23 +5,26 @@ namespace Puffin.Core.Ecs.Components
 {
     /// <summary>
     /// Allows an entity to play an audio file (short or long), optionally at a modified pitch.
-    /// You should be able to play WAV files and OGG files.
+    /// You should be able to play WAV files, OGG files, and MP3 files.
     /// </summary>
     public class AudioComponent : Component
     {
-        internal readonly string FileName;
-        internal float Pitch = 0;
-        public float Volume { get {
-            return _volume;
+        public float Volume
+        {
+            get
+            {
+                return _volume;
             }
-            set {
+            set
+            {
                 _volume = value;
                 this.Parent.Scene.EventBus.Broadcast(EventBusSignal.VolumeChanged, this);
             }
         }
 
-        private float _volume;
-
+        internal readonly string FileName;
+        internal float Pitch = 0f;
+        private float _volume = 1.0f;
 
         public AudioComponent(Entity parent, string fileName) : base(parent)
         {
@@ -33,21 +36,27 @@ namespace Puffin.Core.Ecs.Components
         /// </summary>
         /// <param name="volume">A volume of 1.0 is 100%; a volume of 0 is 0% (completely muted).</param>
         /// <param name="pitch">A pitch of 0 is 100% (normal); -1 plays at half the pitch, 1 plays at double pitch.</param>
-        public void Play(float volume = 1.0f, float pitch = 0f)
+        public void Play(float? volume = null, float? pitch = null)
         {
-            if (volume < 0 || volume > 1)
+            if (volume.HasValue)
             {
-                throw new ArgumentException("Volume must be in the range [0..1]");
+                if (volume.Value < 0 || volume.Value > 1)
+                {
+                    throw new ArgumentException("Volume must be in the range [0..1]");
+                }
+                _volume = volume.Value;
             }
 
-            if (pitch < -1 || pitch > 1)
+            if (pitch.HasValue)
             {
-                throw new ArgumentException("Pitch must be in the range [-1..1].");
+                if (pitch.Value < -1 || pitch.Value > 1)
+                {
+                    throw new ArgumentException("Pitch must be in the range [-1..1].");
+                }
+                this.Pitch = pitch.Value;
             }
 
             this.Parent.Scene.EventBus.Broadcast(EventBusSignal.PlayAudio, this);
-            this.Volume = volume;
-            this.Pitch = pitch;
         }
 
         /// <summary>
